411e23e0f4a47d4b7d6900b780d49df6bf6736a2
Данило <@LAPTOP-7M1VSVI1>
2025-12-28 16:32:38 +0200

    Добавьте файлы проекта.

---
+++ cpp-calculator/cpp-calculator.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+int main()
+{
+    std::string math_e;
+    std::cout << "Enter a math problem: \n" << std::endl;
+    std::cin >> math_e;
+
+    std::vector<char> tokens;
+
+    return 0;
+}
