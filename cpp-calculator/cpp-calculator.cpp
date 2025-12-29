2c8c209eafe33fee9431f1f2fd431e775346f6b1
Данило <@LAPTOP-7M1VSVI1>
2025-12-29 18:13:45 +0200

    feat: implement prototype of project and Shunting-yard algorithm.

--- cpp-calculator/cpp-calculator.cpp
+++ cpp-calculator/cpp-calculator.cpp
@@ -1,15 +1,34 @@
 ﻿#include <iostream>
-#include <string>
-#include <vector>
 #include <stack>
+#include <queue>
+#include <string>
 
 int main()
 {
     std::string math_e;
-    std::cout << "Enter a math problem: \n" << std::endl;
+    std::cout << "Enter a math problem: " << std::endl;
     std::cin >> math_e;
 
-    std::vector<char> tokens;
+    std::vector<char> output_arr;
+    std::stack<char> operators_stack;
+
+    for (auto it = math_e.begin(); it != math_e.end(); it++) {
+        if (*it == '+' || *it == '-' || *it == '/' || *it == '*') {
+            while (!operators_stack.empty() && (operators_stack.top() == '*' || operators_stack.top() == '/')) {
+                output_arr.push_back(operators_stack.top());
+                operators_stack.pop();
+            }
+            operators_stack.push(*it);
+            continue;
+        }
+        output_arr.push_back(*it);
+    }
+    std::cout << '\n';
+   
+    while (!operators_stack.empty()) {
+        output_arr.push_back(operators_stack.top());
+        operators_stack.pop();
+    }
 
     return 0;
 }
