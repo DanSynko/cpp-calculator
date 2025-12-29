5eb9cad52bef9243df0817de798432cc1205986e
Данило <@LAPTOP-7M1VSVI1>
2025-12-29 23:33:42 +0200

    feat: implement simplest calculator.

--- cpp-calculator/cpp-calculator.cpp
+++ cpp-calculator/cpp-calculator.cpp
@@ -30,5 +30,55 @@ int main()
         operators_stack.pop();
     }
 
+
+
+
+
+    std::stack<char> stack_temp = operators_stack;
+
+    std::cout << "reverse polish notation: ";
+    for (auto it = output_arr.begin(); it != output_arr.end(); it++) {
+        std::cout << *it << ". ";
+    }
+    while (!stack_temp.empty()) {
+        std::cout << stack_temp.top() << ". " << std::endl;
+        stack_temp.pop();
+    }
+    std::cout << '\n' << std::endl;
+
+
+
+
+    std::stack<int> result;
+
+    for (auto it = output_arr.begin(); it != output_arr.end(); it++) {
+        if (*it == '+' || *it == '-' || *it == '*' || *it == '/') {
+            int a = result.top();
+            result.pop();
+            int b = result.top();
+            result.pop();
+            switch (*it) {
+                case '+':
+                    result.push(a + b);
+                    break;
+                case '-':
+                    result.push(b - a);
+                    break;
+                case '*':
+                    result.push(a * b);
+                    break;
+                case '/':
+                    result.push(b / a);
+                    break;
+            }
+        }
+        else if (isdigit(*it)) {
+            result.push(*it - '0');
+        }
+    }
+
+
+    std::cout << "result: " << result.top() << std::endl;
+
     return 0;
 }
