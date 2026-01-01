1159e8216837c7fdeba203fafc00cc9dd792e846
Данило <@LAPTOP-7M1VSVI1>
2026-01-01 20:35:03 +0200

    feat: add support for brackets () and protection against invalid input. Make math_expression_validation() a template to enable validation of expressions with brackets.

--- cpp-calculator/cpp-calculator.cpp
+++ cpp-calculator/cpp-calculator.cpp
@@ -6,7 +6,7 @@ void safe_input(std::string& expr) {
     std::getline(std::cin, expr);
 
     // this list in string_view will change
-    // TODO: add (), %, ^.
+    // TODO: add %, ^.
     
     bool correct_expr;
     do {
@@ -16,7 +16,7 @@ void safe_input(std::string& expr) {
         }
         else {
             for (auto it = expr.begin(); it != expr.end(); it++) {
-                if (std::string_view("0123456789+-*/., ").find(*it) == std::string_view::npos) {
+                if (std::string_view("0123456789+-*/.,() ").find(*it) == std::string_view::npos) {
                     correct_expr = false;
                     break;
                 }
@@ -32,13 +32,25 @@ void safe_input(std::string& expr) {
 }
 
 
-
-bool math_expression_validation(const std::vector<std::string>& expr) {
+template<typename T>
+concept mathexpr = std::same_as<T, std::string> ||
+std::same_as<T, std::vector<std::string>>;
+bool math_expression_validation(const mathexpr auto& expr) {
     int operators_count = 0;
     int operands_count = 0;
+    int open_brackets_count = 0;
+    int close_brackets_count = 0;
     for (auto it = expr.begin(); it != expr.end(); it++) {
         if (std::string_view("+-*/").find(*it) != std::string_view::npos) {
             operators_count++;
+        }
+        else if (std::string_view("()").find(*it) != std::string_view::npos) {
+            if (std::string_view("(").find(*it) != std::string_view::npos) {
+                open_brackets_count++;
+            }
+            else if (std::string_view(")").find(*it) != std::string_view::npos) {
+                close_brackets_count++;
+            }
         }
         else if (std::string_view(".").find(*it) != std::string_view::npos) {
             continue;
@@ -55,6 +67,9 @@ bool math_expression_validation(const std::vector<std::string>& expr) {
     else if (operands_count <= operators_count) {
         return false;
     }
+    else if (open_brackets_count != close_brackets_count) {
+        return false;
+    }
 
     return true;
 }
@@ -68,15 +83,28 @@ std::vector<std::string> shunting_yard_algorithm(const std::string& expr) {
     for (auto it = expr.begin(); it != expr.end(); it++) {
         if (*it == ' ') continue;
 
-        if (std::string_view("+-*/").find(*it) != std::string_view::npos) {
-            if (*it == '/' || *it == '*') {
+        if (std::string_view("+-*/()").find(*it) != std::string_view::npos) {
+            if (*it == ')') {
+                while (!operators_stack.empty() && (operators_stack.top() != '(')) {
+                    output_arr.push_back(std::string(1, operators_stack.top()));
+                    operators_stack.pop();
+                }
+                if (!operators_stack.empty()) {
+                    operators_stack.pop();
+                    continue;
+                }
+                else {
+                    return output_arr;
+                }
+            }
+            else if (*it == '/' || *it == '*') {
                 while (!operators_stack.empty() && (operators_stack.top() == '*' || operators_stack.top() == '/')) {
                     output_arr.push_back(std::string(1, operators_stack.top()));
                     operators_stack.pop();
                 }
             }
             else if (*it == '+' || *it == '-') {
-                while (!operators_stack.empty()) {
+                while (!operators_stack.empty() && operators_stack.top() != '(') {
                     output_arr.push_back(std::string(1, operators_stack.top()));
                     operators_stack.pop();
                 }
@@ -110,7 +138,7 @@ std::vector<std::string> shunting_yard_algorithm(const std::string& expr) {
         output_arr.push_back(std::string(1, operators_stack.top()));
         operators_stack.pop();
     }
-
+    
     return output_arr;
 }
 
@@ -161,7 +189,7 @@ int main()
     bool incorrect_expr = true;
     while (incorrect_expr) {
         RPN = shunting_yard_algorithm(math_e);
-        if (math_expression_validation(RPN)) {
+        if (math_expression_validation(math_e) && math_expression_validation(RPN)) {
             incorrect_expr = false;
         }
         else {
@@ -171,6 +199,10 @@ int main()
     }
 
     std::cout << '\n';
+
+
+
+
 
 
 
